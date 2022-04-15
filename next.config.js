49dc5787a9b651f50721ade8f1b916c24593a5da
sh-tech-1 <cc89mk@gmail.com>
2022-04-16 02:27:52 +0900

    move src folder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/src/pages",
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
