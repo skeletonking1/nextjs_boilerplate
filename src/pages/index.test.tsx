b9caf57917ede818972b58e9a88e5aca252d3c41
skeletonking1 <blue.dragon980511@gmail.com>
2022-04-18 17:06:02 -0700

    updated test cases

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from '.';
+
+describe('<Home/>', () => {
+  it('should render correctly', () => {
+    const { container } = render(<Home />);
+
+    expect(screen.getByRole('img', { name: /welcome/i })).toBeInTheDocument;
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
